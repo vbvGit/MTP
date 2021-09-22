f6d5d849be772e2b2d7aeaca5a11811cde432dbe
Riya Samanta <riya.samanta@iitkgp.ac.in>
2021-09-22 22:11:57 +0530

    correct file

--- VTM/OG.py
+++ VTM/OG.py
@@ -2,36 +2,19 @@ import numpy as np
 import pandas as pd
 import random
 import statistics
-import WCB
-import sys
-import time
+
 
 class SIM():
     def __init__(self):
-       self.currCandidates = list()
-       self.currTasks = list()
-       self.taskCandidateMapper = {}
-       self.clock = 0
-       self.time = 10
-       self.task_noBudget = dict()
-       self.task_timeout = set()
-       self.candidate_noBudget = dict()
-       self.round=1
-       cols1=["rounds","candidates","tags","potential","payments","alpha","l","ageingFactor","p_init","elected"]
-       self.potDt=pd.DataFrame(columns=cols1)
-       cols2=["rounds","tasks","budgets","completed"]
-       self.taskStat=pd.DataFrame(columns=cols2)
-        # self.completedTasks={}
-       self.status={}
-       self.leftBudgets={}
-       self.candPay={}
-       self.candElected={}
-       self.potDt['alpha']=0
-       self.potDt['l']=0
-       self.potDt['ageingFactor']=0
-       self.potDt["p_init"]=0
-        
-        
+        self.currCandidates = list()
+        self.currTasks = list()
+        self.taskCandidateMapper = {}
+        self.clock = 0
+        self.time = 10
+        self.task_noBudget = dict()
+        self.task_timeout = set()
+        self.candidate_noBudget = dict()
+
     def getUnarrivedCandidates(self):
         unarrivedCandidates = []
         for candidate in self.currCandidates :
@@ -47,21 +30,21 @@ class SIM():
         return unarrivedTasks
     
     
-    def startUp(self, C, T,size):
+    def startUp(self, C, T):
         # for adding the C and T
         # C is set of Candidates which are stored :: numpy
         # T is set of Task which are stored   :: numpy
 
         # adding the candidates
-        # size, _ = C.shape
+        size, _ = C.shape
         for c in range(size):
             data = list(C[c])
             newCandidate = Candidate(data)
             self.currCandidates.append(newCandidate)
 
         # adding the tasks
-        sizeT, _ = T.shape
-        for t in range(sizeT):
+        size, _ = T.shape
+        for t in range(size):
             data = list(T[t])
             newTask = Task(data)
             self.currTasks.append(newTask)
@@ -112,16 +95,15 @@ class SIM():
         reward_perHour = candidate.price
         duration_totalHour = task.duration
         net_reward=(reward_perHour*duration_totalHour)
-        taskBudget = task.remaningBudget
-        return (taskBudget-net_reward), matchedSkillTask, matchedSkillCandidate
+        if(len(matchedSkillTask)):
+            return (net_reward/len(matchedSkillTask)), matchedSkillTask, matchedSkillCandidate
+        else:
+            return 0, matchedSkillTask,matchedSkillCandidate
+         
         
 
 
     def updateWorkingCandidatesStatus(self):
-        candList=[]
-        tags=[]
-        plevels=[]
-        rounds=[]
         # status : idle and hasBudget
         for candidate in self.currCandidates:
             if candidate.isIdle == False:
@@ -129,18 +111,6 @@ class SIM():
             # when candidate is assigning some task ,clock_taskCompletion  will be setted at that time
             elif self.clock >= candidate.clock_taskCompletion:
                 candidate.isIdle = True
-                pLevel,r=0,self.round
-            # print("test----pLevel  ",pLevel)
-            tags.append(candidate.tag)
-            plevels.append(pLevel)
-            rounds.append(r)
-            candList.append(candidate.name)
-            
-        self.potDt["candidates"]=candList
-        self.potDt["tags"]=tags
-        self.potDt["potential"]=plevels
-        self.potDt["rounds"]=rounds
-           
 
     def getAssignableCandidates(self):
         assignableCandidate = []
@@ -162,7 +132,6 @@ class SIM():
         candidateCharge = candidate.price
         taskDuration = task.duration
         priceRequiredforCandidate = candidateCharge*taskDuration
-        self.candPay[candidate.name]=priceRequiredforCandidate
         return priceRequiredforCandidate
 
     def getEligibleCandidatesForTask(self, task, candidates):
@@ -193,7 +162,6 @@ class SIM():
                 self.taskCandidateMapper[key][2] = True  # task completed
                 self.taskCandidateMapper[key][5] = waitingTime
                 print("One task is completed")
-                self.status[key]=1
 
     def getAssignableTasks(self):
         # get the list of task which can be asignable from curr logical time
@@ -238,9 +206,6 @@ class SIM():
             if min_util_fact < utilFact:
                 min_util_fact = utilFact
                 electedCandidate = e
-                self.candElected[e.name]=1
-            else:
-                  self.candElected[e.name]=0
         return electedCandidate
 
     def update(self, selectionPolicy):
@@ -289,7 +254,6 @@ class SIM():
 
                 # selectedCandidate.skills=self.trimUnwanteds(selectedCandidate.skills,matchedSkillCandidate)
                 electedCandidate.clock_taskCompletion = self.clock + task.duration
-                self.leftBudgets[task.name]=task.remaningBudget
 
             else:
                 # assignment is not possible
@@ -356,131 +320,15 @@ class SIM():
 
     def run(self, time=50):
         self.time = time
-        j=0
-        self.taskStat["tasks"]=[x.name for x in self.currTasks ]
-        tot_ca=0
-        budgetLeft=0
-        cols=["candidates","elected"]
-        caList=[]
-        budgetList=[]
-        initialBudget=[]
-        idx=0
-        elected_prev=pd.DataFrame(columns=cols)
         for i in range(self.time):
             self.update(self.selection_OUR)
-            j=j+1
-
-            if j==50:
-                print("    ",self.round,"---round completes")
-                
-                self.taskStat['completed']=self.taskStat.tasks.astype(str).map(self.status)
-                self.taskStat['budgets']=self.taskStat.tasks.astype(str).map(self.leftBudgets)
-                
-                
-                self.potDt["payments"]=self.potDt.candidates.astype(str).map(self.candPay)
-                self.potDt["elected"]=self.potDt.candidates.astype(str).map(self.candElected)
-                
-                
-                self.potDt.fillna(0, inplace=True)
-                self.taskStat.fillna(0, inplace=True)
-                
-                # # Remove any row with all zeroes
-                series = (self.potDt != 0).any(axis=1)
-                self.potDt=self.potDt.loc[series]
-                self.potDt.to_excel("test.xlsx",index=False)
-
-                # print("shape is--",self.potDt.shape)
-                 # Total number of rounds that have happened
-                tot_num_of_rounds = (self.potDt.iloc[0,0])
-                
-                elected_curr=self.potDt[["candidates","elected"]]
-               
-                
-                # Get the remaining budget from the completed tasks
-                task_status=self.taskStat.shape[1]-1
-                bd=[]   
-                # print("*****Task shape****",self.taskStat.shape)
-                for i in range(0,self.taskStat.shape[0]):
-                    if self.taskStat.iloc[i,task_status]==1:
-                        bd.append(float(self.taskStat.loc[i,"budgets"]))
-                        # print("Appended budget-->",bd)
-
-
-                budgetLeft=sum(bd)
-                initialBudget.append(budgetLeft)
-                # print("Initial budget-->",budgetLeft)
-                
-                
-                self.leftBudgets={}
-                self.status={}
-                self.candElected={}
-                self.candPay={}
-                
-                CA,retained, dropOuts,_ = WCB.naiveRetention(tot_num_of_rounds,self.potDt,self.taskStat,elected_curr,elected_prev,budgetLeft)
-                
-                curCan=[]
-                curCan=self.currCandidates.copy()
-                for i in dropOuts:
-                    # print(i)
-                    curCan.pop(i)
-                    curCan.insert(i,-1)
-                #     self.currCandidates.pop(i)
-                
-                print("Current candidates-->",len(self.currCandidates)-len(dropOuts))
-                
-                idx_= 0
-                for i in range(0,len(curCan)):
-                    if curCan[i]!=-1:
-                        # print("*******",curCan[i])
-                        self.currCandidates[idx_]=curCan[i]
-                        idx_=idx_+1
-                idx=idx+idx_
-                        
-                        
-                cols=["retained","pot","ca"]
-                results=pd.DataFrame(columns=cols)
-                results["retained"]=retained
-                results["pot"]=0
-                results["ca"]=results.retained.map(CA)
-                
-                
-                print("Drop outs---->",len(dropOuts))
-                print("Retained  -->",len(retained))
-                #Removing the drop outs 
-                # for i in dropOuts:
-                #     # self.currCandidates=np.delete(self.currCandidates,i-1,0)
-                #     self.potDt.drop(i,inplace=True)
-                
-                # print("current candidates counts--->",len(self.potDt))
-                tot_ca=sum(results["ca"])  
-                print("ca incurred---->",tot_ca)
-                caList.append(tot_ca)
-                
-                results.to_excel("results//rest//WCB_results"+str(self.time)+"_"+str(self.round)+".xlsx",index=False)
-                # self.potDt.to_excel("results//sigmoid//candidates_potential_Sig_"+str(time)+"_"+str(self.round)+".xlsx",index=False)
-                # self.potDt.to_excel("results//normal//candidates_potential_Norm_"+str(time)+"_"+str(self.round)+".xlsx",index=False)
-                # self.taskStat.to_excel("results//normal//task_status_Norm_"+str(time)+"_"+str(self.round)+".xlsx",index=False)
-                # self.taskStat.to_excel("results//sigmoid//task_status_Sig_"+str(time)+"_"+str(self.round)+".xlsx",index=False)
-                
-                elected_prev=elected_curr
-                
-                self.round=self.round+1
-                j=0
-                bLeft=budgetLeft-tot_ca
-                budgetList.append(bLeft)
-
 
             self.clock = self.clock + 1
-            
             print(i)
-        print("Initial budgets--->",initialBudget)
-        print("Final budget after this round-->",budgetList)
-        print("Final CA--->",caList)
       
         ag.summary()
-        
-        
-        
+
+
 class Task():
     def __init__(self, data):
         self.name = data[0]
@@ -491,9 +339,9 @@ class Task():
         self.skillsBudget = float(data[5])
         self.remaningBudget = float(data[5])
         self.duration = int(data[6])
-        self.arrivalTime = 1
+        #self.arrivalTime = 1
 
-        # self.arrivalTime = int(data[7])
+        self.arrivalTime = int(data[7])
         self.fTime = 200
         self.isCompleted = False
 
@@ -501,38 +349,30 @@ class Task():
 class Candidate():
     def __init__(self, data):
         self.name = data[0]
-        self.skills = data[3]
-        self.price = float(data[1])
-        self.sucessRate = float(data[4])
-        self.skillCount = int(data[2])
-        self.rewardPerSkill = int(data[10])
-        self.bias = float(data[7])
-        self.normSucessRate = float(data[9])
+        self.skills = data[1]
+        self.price = float(data[2])
+        self.sucessRate = float(data[3])
+        self.skillCount = int(data[4])
+        self.rewardPerSkill = int(data[5])
+        self.bias = float(data[6])
+        self.normSucessRate = float(data[7])
         if self.normSucessRate != self.normSucessRate:
             self.normSucessRate = 0.001
 
         self.duration = int(data[8])
-        self.arrivalTime = 1
-        # self.arrivalTime = int(data[12])
+        #self.arrivalTime = 1
+        self.arrivalTime = int(data[9])
 
         self.isIdle = True
         self.utilisedTime = 0
         self.clock_taskCompletion = 0
-        self.successRatio=0
-        self.userRating=float(data[5])
-        self.alpha=float(data[6])
-        self.tag=data[11]
-        
-tasks = pd.read_excel('datasets//sample_tasks_updated.xlsx').to_numpy()
-candidates = pd.read_excel('datasets//merged_candidates.xlsx').to_numpy()
-print ("Runs =  ",sys.argv[1])
-size=int(sys.argv[2])
-print("Size of candidaters == ",size)
 
-start_time=time.time()
+
+tasks = pd.read_excel('sample_tasks_updated.xlsx').to_numpy()
+candidates = pd.read_excel('sample_candidates.xlsx').to_numpy()
 
 ag = SIM()
-ag.startUp(candidates, tasks,size)
+ag.startUp(candidates, tasks)
 ag.clock = 1
 c = ag.getAssignableCandidates()
 print(len(c))
@@ -542,8 +382,4 @@ print(len(t))
 # print(len(ect))
 # ag.update(ag.selection_random)
 # print(ag.taskCandidateMapper)
-ag.run(int(sys.argv[1]))
-
-end_time=time.time()
-
-print("Exceution time--->",end_time-start_time)
+ag.run(125)
