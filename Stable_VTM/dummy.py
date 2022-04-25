dfc07c6eeefb1d1133ab669709908d661604bc0c
vbvGit <87233932+vbvGit@users.noreply.github.com>
2022-04-25 10:37:21 +0530

    Delete Stable_VTM directory

--- Stable_VTM/dummy.py
+++
@@ -1,7 +0,0 @@
-import math
-task = ["t1","t2","t3","t4","t5","t6"]
-locs = [(1,2),(3,5),(5,5),(5,8),(3,1),(6,10)]
-taskData = dict(zip(task,locs))
-sortedTaskData = {k: v for k, v in sorted(taskData.items(), key=lambda i: math.sqrt((i[1][0]-13)**2 + (i[1][1]-17)**2))}
-print(sortedTaskData.keys())
-
